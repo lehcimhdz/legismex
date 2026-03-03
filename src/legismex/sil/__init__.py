3e8c1edaeaf7b63820f11240848a5ca20e8baefe
lehcimhdz <bmichelcano@gmail.com>
2026-03-02 18:11:18 -0600

    refactor: drop SIL logic and move to Gaceta Parlamentaria scope

--- src/legismex/sil/__init__.py
+++
@@ -1,5 +0,0 @@
-from .client import SILClient
-from .models import Legislador, IniciativaResumen, IniciativaDetalle, ReporteSesionResumen, ReporteSesionDetalle
-from .parser import SILParser
-
-__all__ = ["SILClient", "Legislador", "IniciativaResumen", "IniciativaDetalle", "ReporteSesionResumen", "ReporteSesionDetalle", "SILParser"]
